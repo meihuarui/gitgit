96ad7a9d899aaf3828646acc0ffb4fb93007c900
weichaoyang <naotengnaoteng@163.com>
2018-12-23 23:52:57 +0800

    cache

--- springboot-01-cache/src/test/java/com/atguigu/cache/Springboot01CacheApplicationTests.java
+++ springboot-01-cache/src/test/java/com/atguigu/cache/Springboot01CacheApplicationTests.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -14,6 +16,23 @@ public class Springboot01CacheApplicationTests {
 
     @Autowired
     private EmployeeMapper employeeMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;//操作字符串的
+
+    @Test
+    public void test2() throws Exception{
+        String k3 = stringRedisTemplate.opsForValue().get("k3");
+        System.out.println(k3);
+    }
+
+    @Test
+    public void test() throws Exception{
+        stringRedisTemplate.opsForValue().append("k3","git redis");
+    }
 
     @Test
     public void contextLoads() {
